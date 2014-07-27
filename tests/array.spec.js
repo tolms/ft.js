6a2cca3a3fa4188c4c7632786774b118e8ed9367
instanceofpro <instanceofpro@gmail.com>
2014-07-27 19:36:50 +0600

    test passes

--- tests/array.spec.js
+++ tests/array.spec.js
@@ -1,7 +1,7 @@
 describe('Array', function () {
     describe('.value()', function () {
         it('should return value', function () {
-            expect(ft.array(1).value()).to.equal(11);
+            expect(ft.array(1).value()).to.equal(1);
         });
     });
 });
