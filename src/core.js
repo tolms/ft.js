802590cae69be492659529f525de1d37325a5df1
instanceofpro <instanceofpro@gmail.com>
2014-08-18 21:01:03 +0600

    update UMD header

--- src/core.js
+++ src/core.js
@@ -1,8 +1,6 @@
 (function (root, factory) {
-    if (typeof define === 'function' && typeof define.amd === 'object') {
-        define(function () {
-            return factory();
-        });
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
     } else if (typeof exports !== 'undefined') {
         module.exports = factory();
     } else {
