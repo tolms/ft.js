13c0a39df751c714f946852bc15fe4371d7ba443
instanceofpro <instanceofpro@gmail.com>
2014-11-01 23:40:34 +0500

    update tests for ft.string.repeat

--- tests/string.spec.js
+++ tests/string.spec.js
@@ -1,13 +1,32 @@
 describe('#string()', function () {
     describe('.repeat()', function () {
-        it('#string().repeat()', function () {
-            var one = ft.string('One!');
+        var one;
 
+        before(function () {
+            one = ft.string('One!');
+        });
+
+        it('#string(\'One!\').repeat(4) should be \'One!One!One!One!\'', function () {
             expect(one.repeat(4)).to.equal('One!One!One!One!');
+        });
+
+        it('#string(\'One!\').repeat() should be empty string', function () {
             expect(one.repeat(0)).to.equal('');
+        });
+
+        it('#string(\'One!\').repeat() should be empty string', function () {
             expect(one.repeat(NaN)).to.equal('');
+        });
+
+        it('#string(\'One!\').repeat() should be empty string', function () {
             expect(one.repeat(null)).to.equal('');
+        });
+
+        it('#string(\'One!\').repeat() should be empty string', function () {
             expect(one.repeat()).to.equal('');
+        });
+
+        it('#string(\'One!\').repeat(\'2\') should be \'One!One!\'', function () {
             expect(one.repeat('2')).to.equal('One!One!');
         });
     });
