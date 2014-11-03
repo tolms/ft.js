9ceaa39be1c0574560da8ce33050a81e26c7b253
instanceofpro <instanceofpro@gmail.com>
2014-11-03 20:02:57 +0500

    add ft.object.get

--- src/object.js
+++ src/object.js
@@ -23,14 +23,46 @@ var Objects = (function () {
             throw new Error();
         },
 
+        /**
+         * Метод проверяет наличие ключа в объекте
+         * @param key Имя ключа
+         * @returns {Boolean}
+         */
         has: function (key) {
             return _.has.call(this._value, key);
         },
 
+        /**
+         * Метод возвращает массив ключей исходного объекта
+         * @returns {Array}
+         */
         keys: function () {
             return Object.keys(this._value);
         },
 
+        get: function (prop) {
+            var obj = this._value,
+                parts = prop.split('.'),
+                last = parts.pop();
+
+            while (prop = parts.shift()) {
+                obj = obj[prop];
+                if (!ft.is(obj).exists()) {
+                    return;
+                }
+            }
+
+            return obj[last];
+        },
+
+        set: function (path, value) {
+
+        },
+
+        /**
+         * Метод возвращает массив пар ключ-значение от исходного объекта
+         * @returns {Array}
+         */
         pairs: function () {
             var that = this;
             return _.map(this.keys(), function (el) {
@@ -43,6 +75,10 @@ var Objects = (function () {
             throw new Error();
         },
 
+        /**
+         * Метод возвращает массив значений исходного объекта
+         * @returns {Array}
+         */
         values: function () {
             var that = this;
             return _.map(this.keys(), function (el) {
--- tests/object.spec.js
+++ tests/object.spec.js
@@ -1,5 +1,34 @@
 describe('#object()', function () {
-    describe('.value()', function () {
-        it('should return value');
+    describe('.get()', function () {
+        var lorem, foo;
+
+        before(function () {
+            lorem = {
+                lorem: {
+                    ipsum: {
+                        dolor: {
+                            sit: 'amet'
+                        }
+                    }
+                }
+            };
+            foo = {
+                foo: {
+                    foo: {
+                        foo: 'foo'
+                    }
+                }
+            };
+        });
+
+        it('should return value of nested property', function () {
+            expect(ft.object(lorem).get('lorem.ipsum.dolor.sit')).to.equal('amet');
+            expect(ft.object(foo).get('foo.foo')).to.deep.equal({ foo: 'foo' });
+        });
+
+        it('should not return value of undefined property', function () {
+            expect(ft.object(lorem).get('lorem.ipsum.foo')).to.be.undefined;
+            expect(ft.object(foo).get('foo.foo.foo.foo.foo.foo')).to.be.undefined;
+        });
     });
 });
